74e0e0c6045ecaef3488b59039ff9f3b515601ea
6lood1ess <6lo.od1ess@gmail.com>
2026-02-26 17:33:01 +0700

    - Created the ZooManager class, implemented as Singleton. It is responsible for: 1. Keeping a list of animals; 2. Derivation of information for all animals or a specific animal by index; 3. Provide a menu to interact with the user through the console.
    Will be added to ZooManager:
    *Method of creating a new animal;
    *The hardcoding method of creating demonstration animals.

--- Program.cs
+++ Program.cs
@@ -117,3 +117,151 @@ class Amphibian : Animal {
     }
   }
 }
+
+class ZooManager {
+
+  private static ZooManager _instance;
+
+  public List<Animal> animalInventory = new List<Animal> { };
+
+  public void AddAnimalToInventory(Animal animal) {
+
+    animalInventory.Add(animal);
+    Console.WriteLine($"\nAnimal {animal.Name} added to the zoo :)");
+  }
+
+  public static ZooManager Instance {
+
+    get {
+
+      if (_instance == null) {
+
+        _instance = new ZooManager();
+      }
+
+      return _instance;
+    }
+  }
+
+  public void ShowAllAnimals() {
+
+    if (animalInventory.Count == 0) {
+
+      Console.WriteLine("There are no animals in the zoo yet :(");
+      return;
+    }
+
+    string[] title = new string[] { "-=_8-8__|- OUR ANIMALS -|__8-8_=-" };
+    Program.MoveTextToCenter(title);
+
+    Console.WriteLine("");
+
+    for (int animalIndex = 0; animalIndex < animalInventory.Count; ++animalIndex) {
+
+      Console.WriteLine($"Animal №{animalIndex + 1}:\n\n" +
+                        $"{animalInventory[animalIndex].GetInfo()}\n" +
+                        "-------------------------------------------------------");
+    }
+  }
+
+  public void GetAnimalByIndex(int animalIndex) {
+
+    if (animalIndex < 0 || animalIndex >= animalInventory.Count) {
+
+      Console.WriteLine("Wrong animal index! >:O");
+      return;
+    }
+
+    Console.WriteLine($"\nAnimal №{animalIndex + 1}:\n\n" +
+                      $"{animalInventory[animalIndex].GetInfo()}\n");
+  }
+
+  public void RunMainMenu() {
+
+    ZooManager zoo = ZooManager.Instance;
+
+    bool isRunning = true;
+
+    while (isRunning) {
+
+      Console.WriteLine("\n\n\n\n\n\n\n");
+            
+      string[] mainMenuControls = new string[] {
+
+        "- ZOO MENU -",
+        "",
+        "1 - show all animals",
+        "---------------------",
+        "2 - add new animal",
+        "---------------------",
+        "3 - find animal by index",
+        "---------------------",
+        "4 - exit"
+      };
+      Program.MoveTextToCenter(mainMenuControls);
+
+      string choice;
+      Console.Write("\n\nPlease enter the command number (1 to 4): ");
+      choice = Console.ReadLine();
+
+      switch (choice) {
+
+        case "1": {
+
+          Console.Clear();
+          zoo.ShowAllAnimals();
+          break;
+        }
+
+        case "2": {
+
+          Console.Clear();
+          CreateAnimal(zoo);
+          break;
+        }
+
+        case "3": {
+
+          Console.Clear();
+
+          Console.Write("Enter animal index (1, 2, 3, etc.): ");
+
+          if (int.TryParse(Console.ReadLine(), out int animalIndex)) {
+
+            zoo.GetAnimalByIndex(animalIndex - 1);
+          } else {
+
+            Console.WriteLine("Incorrect animal index! >:O");
+          }
+
+          break;
+        }
+
+        case "4": {
+
+          isRunning = false;
+          break;
+
+        default:
+          Console.WriteLine("Incorrect command number. Try again, please . . .");
+          break;
+        }
+      }
+
+      if (isRunning) {
+
+        Console.Write("\nEnter any key to return to the main menu: ");
+        Console.ReadKey();
+        Console.Clear();
+      }
+    }
+  }
+  
+  public void CreateAnimal(ZooManager zoo) {
+
+  }
+
+  public void CreateDemoAnimals() {
+
+  }
+}
