045cd1a54dd1b295ec636998da43e3e1da0c8695
6lood1ess <6lo.od1ess@gmail.com>
2026-02-26 16:29:13 +0700

    - The parent class of Animal is made. It defines the basic properties on which the constructor and the GetInfo() method, which provides information about a specific animal, are based.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AnimalManager
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+abstract class Animal {
+
+  public string Name { get; set; }
+  public int Age { get; set; }
+  public string Habitat { get; set; }
+  public string TypeOfFood { get; set; }
+  public double Weight { get; set; }
+  public double Length { get; set; }
+  public double Height { get; set; }
+  public string Color { get; set; }
+
+  public Animal(string name, int age, string habitat, string typeOfFood, double weight, double length, double height, string color) {
+
+    Name = name;
+    Age = age;
+    Habitat = habitat;
+    TypeOfFood = typeOfFood;
+    Weight = weight;
+    Length = length;
+    Height = height;
+    Color = color;
+  }
+
+  public virtual string GetInfo() {
+    return $"Name: {Name}; age: {Age} years; habitat: {Habitat};\n" +
+               $"type of food: {TypeOfFood}; weight: {Weight} kg; lenght: {Length} m; height: {Height} m;\n" +
+               $"color: {Color}";
+  }
 }
