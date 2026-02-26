574ecacc4d73db4700d1de8fd3d335994cb11dd3
6lood1ess <6lo.od1ess@gmail.com>
2026-02-26 16:42:14 +0700

    - Added three child classes (Mammal, Bird, Fish) with their individual properties

--- Program.cs
+++ Program.cs
@@ -30,3 +30,48 @@ abstract class Animal {
                $"color: {Color}";
   }
 }
+
+class Mammal : Animal {
+
+  public bool HasFur { get; set; }
+
+  public Mammal(string name, int age, string habitat, string typeOfFood, double weight, double length, double height, string color, bool presenceOfWool): base(name, age, habitat, typeOfFood, weight, length, height, color) {
+
+    HasFur = presenceOfWool;
+  }
+
+  public override string GetInfo() {
+
+    return base.GetInfo() + $"; animal type: mammal; presence of wool: {HasFur}";
+  }
+}
+
+class Bird : Animal {
+
+  public double WingSpan { get; set; }
+
+  public Bird(string name, int age, string habitat, string typeOfFood, double weight, double length, double height, string color, double wingSpan): base(name, age, habitat, typeOfFood, weight, length, height, color) {
+
+    WingSpan = wingSpan;
+  }
+
+  public override string GetInfo() {
+
+    return base.GetInfo() + $"; animal type: bird; wingspan: {WingSpan}";
+  }
+}
+
+class Fish : Animal {
+
+  public string WaterType { get; set; }
+
+  public Fish(string name, int age, string habitat, string typeOfFood, double weight, double length, double height, string color, string waterType): base(name, age, habitat, typeOfFood, weight, length, height, color) {
+
+    WaterType = waterType;
+  }
+
+  public override string GetInfo() {
+
+    return base.GetInfo() + $"; animal type: fish; water type: {WaterType}";
+  }
+}
